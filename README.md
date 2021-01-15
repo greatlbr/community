899376e7760e743725bbdf5f6776c4f6582c3536
greatlbr <lblbr@outlook.com>
2021-01-15 15:26:59 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,22 @@
+# 工程简介
+
+技术栈包括但不限于：
+
+Spring Boot
+
+Spring MVC
+
+MyBatis
+
+Flyway
+
+Heroku
+
+Git/Github
+
+Maven
+
+Restful
+
+# 延伸阅读
+
---
+++ src/test/java/com/comm/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comm.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
