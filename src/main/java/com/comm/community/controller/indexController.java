c998e430108784c862a46afb5056528ec76375f2
greatlbr <lblbr@outlook.com>
2021-01-15 16:37:03 +0800

    add index and bootstrap

---
+++ src/main/java/com/comm/community/controller/indexController.java
@@ -0,0 +1,10 @@
+package com.comm.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+    @GetMapping("/")
+    public String index(){return "index";}
+}
